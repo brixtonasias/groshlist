2e79ceaa073b3066f6db46f0d9c7f41e1c625ea4
Stefan Kohler <stef@peekus.com>
2010-08-14 14:45:56 +0200

    Adjust settings for local environment

--- groshlist/guide/views.py
+++ groshlist/guide/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.conf import settings
+from django.http import HttpResponse
+from django.template import Context, loader
+
+from guide.models import Supermarket
+
+def index(request):
+	markets = Supermarket.objects.all()
+	t = loader.get_template('market/index.html')
+	c = Context ({'object_list' : markets, 'STATIC_URL': settings.STATIC_URL})
+	return HttpResponse(t.render(c))
