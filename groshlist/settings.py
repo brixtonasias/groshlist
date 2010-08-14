dc21e26207fb732bf1e2cb10d35f870bcd8b25c6
Stefan Kohler <stef@peekus.com>
2010-08-14 14:45:02 +0200

    Adjust settings for local environment

--- groshlist/settings.py
+++ groshlist/settings.py
@@ -50,20 +50,19 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '%s/../public/media/' % PROJECT_ROOT
-MEDIA_ROOT = ''
+#MEDIA_ROOT = '%s/../public/media/' % PROJECT_ROOT
+MEDIA_ROOT = '%s/media/' % PROJECT_ROOT
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://media.mymagui.de/'
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
+STATIC_URL = '%sstatic/' % MEDIA_URL
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/admin/'
-ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '0-3ltr%^#*zdwi2mawpx(v0n!ghrv$02@t4f5!8@!rb7ul#^vy'
