428b530ba7311d0c639ec60c2c29c74a0ea9c26e
Stefan Kohler <stef@peekus.com>
2010-08-15 11:50:40 +0200

    add views and forms for login and register

--- groshlist/guide/forms.py
+++ groshlist/guide/forms.py
@@ -3,3 +3,24 @@ from django import forms
 class LoginForm(forms.Form):
 	username = forms.CharField(max_length=100)
 	password = forms.CharField(widget=forms.PasswordInput(render_value=False))
+
+
+class RegisterForm(forms.Form):
+	username = forms.CharField(max_length=100)
+	password = forms.CharField(widget=forms.PasswordInput(render_value=False))
+	password_repeat = forms.CharField(widget=forms.PasswordInput(render_value=False))
+
+
+class SupermarketForm(forms.Form):
+	name = forms.CharField(max_length=255)
+	street = forms.CharField(max_length=255)
+	zipcode = forms.CharField(max_length=10)
+	city = forms.CharField(max_length=255)
+	description = forms.CharField(widget=forms.Textarea)
+
+
+class ShoppingListForm(forms.Form):
+	name = forms.CharField(max_length=255)
+	description = forms.CharField(widget=forms.Textarea)
+	date_created = forms.DateTimeField()
+	date_updated = forms.DateTimeField()
--- groshlist/guide/views.py
+++ groshlist/guide/views.py
@@ -1,16 +1,20 @@
 from django.conf import settings
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render_to_response
 
 from django.core.context_processors import csrf
 
 from guide.models import Supermarket
 from guide.forms import LoginForm
+from guide.forms import RegisterForm
+
 
 def index(request):
-	return render_to_response('site/homepage.html', {'STATIC_URL': settings.STATIC_URL})
+	return render_to_response('site/homepage.html', {'STATIC_URL': settings.STATIC_URL}, RequestContext(request))
 
 
 @login_required(redirect_field_name='/login')
@@ -21,20 +25,55 @@ def market(request):
 	return HttpResponse(t.render(c))
 
 
-def login(request):
+def userlogout(request):
+	logout(request)
+	return render_to_response('site/contact.html')
+
+def userlogin(request):	
 	c = {}
 	c.update(csrf(request))
 	
 	if request.method == 'POST':
 		form = LoginForm(request.POST)
 		if form.is_valid():
-			return render_to_response('site/contact.html', {'STATIC_URL': settings.STATIC_URL})
+			user_name = form.cleaned_data['username']
+			user_pass = form.cleaned_data['password']
+			user = authenticate(username=user_name, password=user_pass)
+			if user is not None:
+				if user.is_active:
+					login(request, user)
+					c.update({'user': user})
+					rc = RequestContext(request, c)
+					print user.is_authenticated()
+					print user.email
+					print "Correct username/password"
+				else:
+					print "Account disabled"
+			else:
+				print "Username + Password incorrect"
+			return render_to_response('site/contact.html', {'STATIC_URL': settings.STATIC_URL}, rc)
 	else:
 		form = LoginForm()
 		
 	c.update({'STATIC_URL': settings.STATIC_URL, 'form': form,})
 		
 	return render_to_response('registration/login.html', c)
+
+
+def register(request):
+	c = {}
+	c.update(csrf(request))
+	
+	if request.method == 'POST':
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+			print 'Form is valid'
+	else:
+		form = RegisterForm()
+		
+	c.update({'STATIC_URL': settings.STATIC_URL, 'form': form,})
+	
+	return render_to_response('registration/register.html', {'form': form, 'STATIC_URL': settings.STATIC_URL})
 
 
 def contact(request):
