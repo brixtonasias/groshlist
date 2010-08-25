110219f5ed9ea7f4f8d609d5030020bd2c12e70c
Stefan Kohler <stef@peekus.com>
2010-08-25 20:30:19 +0200

    Added django-debug-toolbar (middleware, panels, internal ips)

--- groshlist/settings.py
+++ groshlist/settings.py
@@ -82,6 +82,24 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+
+	'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = (
+	'127.0.0.1',
+)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
 )
 
 ROOT_URLCONF = 'groshlist.urls'
@@ -103,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
 	# 3rd party apps
 	'south',
+	'debug_toolbar',
 	# own apps
 	'guide',
 )
